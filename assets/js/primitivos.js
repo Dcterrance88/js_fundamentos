b416ed692dfa502d1311fb35e1ea04c736bd0e81
Dcterrance88 <>
2021-01-11 17:11:43 -0500

    tipos datos primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,29 @@
+let nombre = 'Jhon Coltrain';
+console.log(nombre);
+
+nombre = 'Dani Daniels';
+console.log(nombre);
+
+nombre = "Tio Charlie";
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+let edad = 33.001;
+console.log(typeof edad);
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof symbol1);
+console.log(symbol1 === symbol2);
