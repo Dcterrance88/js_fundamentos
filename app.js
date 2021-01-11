2805d3a4505972298919b3931ad28f4e55e76628
Dcterrance88 <>
2021-01-11 15:18:54 -0500

    Variables y comentarios

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hola mundo desde js');
+// console.log('Hola mundo desde js');
+
+// c = 32 no se puede asignar un nuevo valor a una constante
+// var las variables definidas con var tienen un ámbito de función.Por lo tanto,
+//pueden ser utilizadas en cualquier punto de la función, incluso antes de ser definidas.
+
+//let solo puede ser utilizadas a partir de su definicion en el bloque de codigo.
+let a = 10,
+    b = 10,
+    c = 10,
+    d = 10,
+    x = a + b;
+
+console.log( x );
