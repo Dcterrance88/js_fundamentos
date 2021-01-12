69f6e8349d1ba4af49d8b542b460446dbf08c986
Dcterrance88 <>
2021-01-12 11:19:25 -0500

    Arreglos

---
+++ assets/js/arreglos.js
@@ -0,0 +1,23 @@
+//const arr = new Array(10);
+//const arr = [];
+//console.log(arr);
+
+let videojuegos = ['Mario3', 'Megaman', 'Super turrican'];
+console.log({videojuegos});
+console.log(videojuegos[0]);
+
+let arregloCosa = [
+    true,
+    123,
+    'Terrance',
+    1+2,
+    function(){},
+    ()=>{},
+    {a: 1},
+    ['X', 'Megaman', 0, 'Dr.Light', [
+        'Dr.Willy',
+        'Woodman',
+    ]],
+];
+
+console.log(arregloCosa[7][4][1]);
---
+++ assets/js/arreglos2.js
@@ -0,0 +1,38 @@
+let juegos = ['Zelda', 'Mario', 'Metroid', 'Super Turrican'];
+console.log('Largo:', juegos.length);
+
+let primero = juegos[0];
+primero = juegos[2 - 2];
+let ultimo = juegos[ juegos.length - 1];
+console.log({primero, ultimo});
+
+juegos.forEach((juego, indice, arr) => {
+    console.log('juego: ' + juego);
+    console.log('indice : ' + indice);
+    console.log('arr: ' + arr)
+})
+
+//insertar nuevo elemento en la ultima posicion
+let nuevaLongitud = juegos.push('F-Zero');
+console.log({nuevaLongitud, juegos});
+
+//insertar nuevo elemento en la primera posicion
+nuevaLongitud = juegos.unshift('Fire Embled');
+console.log({nuevaLongitud, juegos});
+
+//borrar ultimo elemento
+let ultimoElementoEliminado = juegos.pop();
+console.log(ultimoElementoEliminado);
+
+//borrar elementos en posicion especifica, el primer parametro especifica la posicion
+//el segundo el numero de elementos que eliminara a partir de la posicion indicada
+//este devuelve un array con los elementos eliminados
+let posicion = 1;
+let cantidadElementos = 2 
+let JuegosEliminados = juegos.splice(posicion, cantidadElementos);
+console.log({JuegosEliminados, juegos});
+
+//Devuelve el indice con respecto al valor del elemento de un arreglo
+//cuando no lo encuentra devuelve un -1
+let superTurricanIndex = juegos.indexOf('Super Turrican');
+console.log(superTurricanIndex);
