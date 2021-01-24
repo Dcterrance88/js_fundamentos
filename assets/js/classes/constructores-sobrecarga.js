becaeae44a54b43968fe37276d34532c39e51b45
Dcterrance88 <>
2021-01-24 15:03:18 -0500

    Multiples Constructores

---
+++ assets/js/classes/constructores-sobrecarga.js
@@ -0,0 +1,31 @@
+class Persona {
+
+    static porObjeto({nombre, apelido, pais}){
+        return new Persona(nombre, apelido, pais);
+    }
+
+    constructor(nombre, apellido, pais) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.pais = pais;
+    }
+
+    getInfo() {
+        console.log(`info: ${this.nombre}, ${this.apellido}, ${this.pais}`);
+    }
+}
+
+const nombre1 = 'Melissa',
+    apellido1 = 'Flores',
+    pais1 = 'Honduras';
+
+const fher = {
+    nombre: 'fernando',
+    apellido: 'Herrera',
+    pais: 'Costa Rica',
+}
+
+const persona1 = new Persona(nombre1, apellido1, pais1);
+const persona2 = Persona.porObjeto(fher);
+persona1.getInfo();
+persona2.getInfo();
